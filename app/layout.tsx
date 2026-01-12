31d525b07d7fc2e7573478e3755129f6fd62e36e
ATREE Bangalore <70751764+atreebangalore@users.noreply.github.com>
2026-01-12 18:24:50 +0530

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Fira_Code } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script";
 
 
 const inter = Inter({
@@ -22,10 +23,28 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Analytics/>
+      
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9WXQDB2KS3"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9WXQDB2KS3', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
 
       <body className={`${inter.variable} ${firaCode.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
